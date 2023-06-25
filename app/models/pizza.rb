c260f00f1c7f67f16133d17c929e5b58656697d8
dMuguti <omiahderek56@gmail.com>
2023-06-25 19:03:02 +0300

    commit

---
+++ app/models/pizza.rb
@@ -0,0 +1,6 @@
+class Pizza < ApplicationRecord
+    has_many :restaurant_pizzas, dependent: :destroy
+    has_many :restaurants, through: :restaurant_pizzas
+  
+    validates :name, :ingredients, presence: true
+  end
